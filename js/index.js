987eb83ce65c75e0a63760c5f632d851df3384f3
jeff-dillon <dillonjg@gmail.com>
2025-01-08 07:32:04 -0600

    completed concept section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+const greeting = "hello"
+
+// greeting = "wassup?"
+
+let salary = "One Million"
+console.log(salary);
+
+*/
